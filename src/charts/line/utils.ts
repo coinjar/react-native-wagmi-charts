4b88e2145ebfa6d84fc77c5e43ceb42e98afe535
Fernando Rojo <frojo@sas.upenn.edu>
2021-10-10 02:56:32 -0400

    Enhance `getDomain` performance (#13)
    Tiny speed upgrade for `getDomain`. FWIW, I've seen the reanimated team recommend using standard `for (let i = 0, ...)` loops on the native thread on their PRs, stating that they're faster. Not trying to overoptimize, but figured I'd mention it.

--- src/charts/line/utils.ts
+++ src/charts/line/utils.ts
@@ -7,7 +7,7 @@ import type { TLineChartData, TLineChartPoint } from './types';
 
 export function getDomain(rows: TLineChartPoint[]): [number, number] {
   'worklet';
-  const values = rows.map(({ value }) => [value]).flat();
+  const values = rows.map(({ value }) => value);
   return [Math.min(...values), Math.max(...values)];
 }
 
