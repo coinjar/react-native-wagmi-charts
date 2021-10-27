8e8e5e9a5a147b7d7d5393eae43b929126e74a14
Marcin Pawlicki <marcin.pawlicki@netguru.com>
2021-10-27 09:13:41 +0200

    fix: at property to be index of data

--- src/charts/line/HorizontalLine.tsx
+++ src/charts/line/HorizontalLine.tsx
@@ -8,6 +8,7 @@ import { Line as SVGLine, LineProps } from 'react-native-svg';
 import { getYForX, parse } from 'react-native-redash';
 
 import { LineChartDimensionsContext } from './Chart';
+import { useLineChart } from './useLineChart';
 
 const AnimatedLine = Animated.createAnimatedComponent(SVGLine);
 
@@ -23,10 +24,14 @@ export const LineChartHorizontalLine = ({
   at = 0,
 }: HorizontalLineProps) => {
   const { width, path } = React.useContext(LineChartDimensionsContext);
+  const { data } = useLineChart();
 
   const parsedPath = React.useMemo(() => parse(path), [path]);
+  const pointWidth = React.useMemo(() => width / data.length, [data]);
 
-  const y = useDerivedValue(() => withTiming(getYForX(parsedPath!, at) || 0));
+  const y = useDerivedValue(() =>
+    withTiming(getYForX(parsedPath!, pointWidth * at) || 0)
+  );
 
   const lineAnimatedProps = useAnimatedProps(() => ({
     x1: 0,
