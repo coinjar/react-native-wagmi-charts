coinjar/react-native-wagmi-charts

commit 6fe5382af60720ebe6ca72930c19d183fe434fce

Add `yRange` option (#21)
* feat & example: y range

* docs: yRange

* make requested changes, move yRange to provider; pass yDomain to context (non-optional, real values)

* docs

* resolve https://github.com/coinjar/react-native-wagmi-charts/pull/21\#discussion_r738008926

* remove type

src/charts/line/utils.ts
@@ -3,7 +3,7 @@ import * as shape from 'd3-shape';
 // @ts-ignore
 import { scaleLinear } from 'd3-scale';
 
-import type { TLineChartData, TLineChartPoint } from './types';
+import type { TLineChartData, TLineChartPoint, YDomain } from './types';
 
 export function getDomain(rows: TLineChartPoint[]): [number, number] {
   'worklet';
@@ -17,20 +17,22 @@ export function getPath({
   height,
   gutter,
   shape: _shape = shape.curveBumpX,
+  yDomain,
 }: {
   data: TLineChartData;
   width: number;
   height: number;
   gutter: number;
   shape?: string;
+  yDomain: YDomain;
 }): string {
-  const timestamps = data.map(({}, i) => i);
-  const values = data.map(({ value }) => value);
+  const timestamps = data.map(({ timestamp }) => timestamp);
+
   const scaleX = scaleLinear()
     .domain([Math.min(...timestamps), Math.max(...timestamps)])
     .range([0, width]);
   const scaleY = scaleLinear()
-    .domain([Math.min(...values), Math.max(...values)])
+    .domain([yDomain.min, yDomain.max])
     .range([height - gutter, gutter]);
   const path = shape
     .line()
@@ -46,20 +48,28 @@ export function getArea({
   height,
   gutter,
   shape: _shape = shape.curveBumpX,
+  yDomain,
 }: {
   data: TLineChartData;
   width: number;
   height: number;
   gutter: number;
   shape?: string;
+  yDomain: YDomain;
 }): string {
-  const timestamps = data.map(({}, i) => i);
-  const values = data.map(({ value }) => value);
+  const timestamps: number[] = [];
+  const values: number[] = [];
+
+  data.forEach(({ value }, index) => {
+    values.push(value);
+    timestamps.push(index);
+  });
+
   const scaleX = scaleLinear()
     .domain([Math.min(...timestamps), Math.max(...timestamps)])
     .range([0, width]);
   const scaleY = scaleLinear()
-    .domain([Math.min(...values), Math.max(...values)])
+    .domain([yDomain.min, yDomain.max])
     .range([height - gutter, gutter]);
   const area = shape
     .area()

example/src/LineChart.tsx
@@ -13,12 +13,40 @@ function invokeHaptic() {
 export default function App() {
   const [data, setData] = React.useState<TLineChartPoint[]>(mockData);
 
+  const [yRange, setYRange] = React.useState<undefined | 'low' | 'high'>(
+    undefined
+  );
+
+  const toggleYRange = () => {
+    setYRange((domain) => {
+      if (!domain) {
+        return 'low';
+      }
+      if (domain === 'low') {
+        return 'high';
+      }
+      return undefined;
+    });
+  };
+
   return (
     <>
       <Heading.H5 paddingX="major-2" marginBottom="major-2">
         Line Chart 📈
       </Heading.H5>
-      <LineChart.Provider data={data}>
+      <LineChart.Provider
+        yRange={{
+          min:
+            yRange === 'low'
+              ? Math.min(...data.map((d) => d.value)) / 1.1
+              : undefined,
+          max:
+            yRange === 'high'
+              ? Math.max(...data.map((d) => d.value)) * 1.1
+              : undefined,
+        }}
+        data={data}
+      >
         <LineChart>
           <LineChart.Path color="red">
             <LineChart.Gradient color="black" />
@@ -33,8 +61,8 @@ export default function App() {
             </LineChart.Tooltip> */}
           </LineChart.CursorCrosshair>
         </LineChart>
-        <Heading.H6>Load Data</Heading.H6>
-        <Box marginTop="major-2">
+        <Box marginX="major-2" marginTop="major-2">
+          <Heading.H6 marginBottom={'major-2'}>Load Data</Heading.H6>
           <Flex flexWrap={'wrap'}>
             <Button onPress={() => setData(mockData)}>Data 1</Button>
             <Button onPress={() => setData(mockData2)}>Data 2</Button>
@@ -68,6 +96,9 @@ export default function App() {
               }
             >
               V large data
+            </Button>
+            <Button onPress={toggleYRange}>
+              {`${yRange || 'Set'} Y Domain`}
             </Button>
           </Flex>
         </Box>

src/charts/line/Chart.tsx
@@ -30,21 +30,35 @@ export function LineChart({
   shape,
   ...props
 }: LineChartProps) {
-  const { data } = useLineChart();
+  const { data, yDomain } = useLineChart();
 
   const path = React.useMemo(() => {
     if (data && data.length > 0) {
-      return getPath({ data, width, height, gutter: yGutter, shape });
+      return getPath({
+        data,
+        width,
+        height,
+        gutter: yGutter,
+        shape,
+        yDomain,
+      });
     }
     return '';
-  }, [data, width, height, yGutter, shape]);
+  }, [data, width, height, yGutter, shape, yDomain]);
 
   const area = React.useMemo(() => {
     if (data && data.length > 0) {
-      return getArea({ data, width, height, gutter: yGutter, shape });
+      return getArea({
+        data,
+        width,
+        height,
+        gutter: yGutter,
+        shape,
+        yDomain,
+      });
     }
     return '';
-  }, [data, width, height, yGutter, shape]);
+  }, [data, width, height, yGutter, shape, yDomain]);
 
   const contextValue = React.useMemo(
     () => ({

src/charts/line/Context.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useSharedValue } from 'react-native-reanimated';
 
-import type { TLineChartContext, TLineChartData } from './types';
+import type { TLineChartContext, TLineChartData, YRangeProp } from './types';
 import { getDomain } from './utils';
 
 export const LineChartContext = React.createContext<TLineChartContext>({
@@ -11,16 +11,22 @@ export const LineChartContext = React.createContext<TLineChartContext>({
   data: [],
   domain: [0, 0],
   isActive: { value: false },
+  yDomain: {
+    min: 0,
+    max: 0,
+  },
 });
 
 type LineChartProviderProps = {
   children: React.ReactNode;
   data: TLineChartData;
+  yRange?: YRangeProp;
 };
 
 export function LineChartProvider({
   children,
   data = [],
+  yRange,
 }: LineChartProviderProps) {
   const currentX = useSharedValue(-1);
   const currentY = useSharedValue(-1);
@@ -29,7 +35,7 @@ export function LineChartProvider({
 
   const domain = React.useMemo(() => getDomain(data), [data]);
 
-  const contextValue = React.useMemo(
+  const contextValue = React.useMemo<TLineChartContext>(
     () => ({
       currentX,
       currentY,
@@ -37,8 +43,21 @@ export function LineChartProvider({
       data,
       isActive,
       domain,
+      yDomain: {
+        min: yRange?.min ?? Math.min(...data.map(({ value }) => value)),
+        max: yRange?.max ?? Math.max(...data.map(({ value }) => value)),
+      },
     }),
-    [currentIndex, currentX, currentY, data, domain, isActive]
+    [
+      currentIndex,
+      currentX,
+      currentY,
+      data,
+      domain,
+      isActive,
+      yRange?.max,
+      yRange?.min,
+    ]
   );
 
   return (

src/charts/line/types.ts
@@ -13,4 +13,15 @@ export type TLineChartContext = {
   data: TLineChartData;
   isActive: Animated.SharedValue<boolean>;
   domain: TLineChartDomain;
+  yDomain: YDomain;
+};
+
+export type YRangeProp = {
+  min?: number;
+  max?: number;
+};
+
+export type YDomain = {
+  min: number;
+  max: number;
 };

commit 2b1f6ae5d8c5aec5b19f421184f1cb7894e834f4

Update utils.ts
Resolve regression mentioned in https://github.com/coinjar/react-native-wagmi-charts/pull/21#discussion_r739821448

src/charts/line/utils.ts
@@ -26,7 +26,7 @@ export function getPath({
   shape?: string;
   yDomain: YDomain;
 }): string {
-  const timestamps = data.map(({ timestamp }) => timestamp);
+  const timestamps = data.map((_, i) => i);
 
   const scaleX = scaleLinear()
     .domain([Math.min(...timestamps), Math.max(...timestamps)])