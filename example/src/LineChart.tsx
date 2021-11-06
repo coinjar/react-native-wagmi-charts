89c7008b4ca1215d2b7b7a060b8e2aeb10d96227
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-06 19:00:33 -0400

    feat: <LineChart.HoverTrap /> component

--- example/src/LineChart.tsx
+++ example/src/LineChart.tsx
@@ -52,7 +52,8 @@ export default function App() {
         onActivated={invokeHaptic}
         onEnded={invokeHaptic}
       >
-        <LineChart.Tooltip />
+        <LineChart.Tooltip position="top" />
+        <LineChart.HoverTrap />
       </LineChart.CursorCrosshair>
     </LineChart>
   );
