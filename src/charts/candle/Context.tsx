bf176aa14564350ae53549377606910dc1e4106e
Tobias Grothmann <Tobias.Grothmann@googlemail.com>
2025-05-14 03:13:51 +0200

    Allow custom Y range (#194)

--- src/charts/candle/Context.tsx
+++ src/charts/candle/Context.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SharedValue, useSharedValue } from 'react-native-reanimated';
 
-import type { TContext, TData } from './types';
+import type { TContext, TData, TDomain } from './types';
 import { getDomain } from './utils';
 
 export const CandlestickChartContext = React.createContext<TContext>({
@@ -21,18 +21,20 @@ type CandlestickChartProviderProps = {
   data: TData;
   width?: number;
   height?: number;
+  valueRangeY?: TDomain;
 };
 
 export function CandlestickChartProvider({
   children,
   data = [],
+  valueRangeY,
 }: CandlestickChartProviderProps) {
   const [width, setWidth] = React.useState(0);
   const [height, setHeight] = React.useState(0);
   const [step, setStep] = React.useState(0);
   const currentX = useSharedValue(-1);
   const currentY = useSharedValue(-1);
-  const domain = React.useMemo(() => getDomain(data), [data]);
+  const domain = React.useMemo(() => valueRangeY ?? getDomain(data), [data, valueRangeY]);
 
   React.useEffect(() => {
     if (data.length) {
