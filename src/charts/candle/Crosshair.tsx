6589f40b4621d6d65339cebfc2eceed471c118b6
Lewis Casewell <lcasewell@moonpay.com>
2025-06-10 09:44:58 +0100

    multiline comment

--- src/charts/candle/Crosshair.tsx
+++ src/charts/candle/Crosshair.tsx
@@ -25,11 +25,17 @@ type MaxDistOverride = LongPressGestureHandlerProps['maxDist'];
 
 type LongPressGestureHandlerOverride = Omit<LongPressGestureHandler, 'minDuration' | 'maxDist'> & {
   /**
-   * Minimum time, expressed in milliseconds, that a finger must remain pressed on the corresponding view. The default value is 0.
+   * Minimum time, expressed in milliseconds, that a finger must remain
+   * pressed on the corresponding view.
+   * @default 0
    */
   minDuration?: MinDurationOverride;
   /**
-   * Maximum distance, expressed in points, that defines how far the finger is allowed to travel during a long press gesture. If the finger travels further than the defined distance and the handler hasn't yet activated, it will fail to recognize the gesture. The default value is 999999.
+   * Maximum distance, expressed in points, that defines how far the finger is
+   * allowed to travel during a long press gesture. If the finger travels
+   * further than the defined distance and the handler hasn't yet activated, 
+   * it will fail to recognize the gesture. 
+   * @default 999999
    */
   maxDist?: MaxDistOverride;
 };
