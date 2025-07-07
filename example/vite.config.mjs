8fe099fd8adfd606f0c4a651a0d7db9615a3fcdb
anthony <anthony@coinjar.com>
2025-07-07 23:23:23 +1000

    cleanup

--- example/vite.config.mjs
+++ example/vite.config.mjs
@@ -35,20 +35,14 @@ export default defineConfig({
     extensions: extensions,
     alias: {
       'react-native': 'react-native-web',
+      'react-native-svg': 'react-native-svg-web',
       'react-native-wagmi-charts': path.resolve(root, 'src'),
     },
   },
   define: {
-    global: 'window',
-    __DEV__: true,
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    'process.env.TAMAGUI_TARGET': JSON.stringify('web'),
-    process: JSON.stringify({
-      env: {
-        NODE_ENV: process.env.NODE_ENV || 'development',
-        TAMAGUI_TARGET: 'web'
-      }
-    }),
+    __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+    'process.env': JSON.stringify(process.env),
+    global: 'globalThis',
   },
   server: {
     port: 3000,
@@ -56,26 +50,22 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    commonjsOptions: { transformMixedEsModules: true },
+    commonjsOptions: { 
+      transformMixedEsModules: true,
+      ignoreDynamicRequires: true,
+    },
   },
   optimizeDeps: {
     include: [
       'react-native-web',
+      'react-native-svg-web',
     ],
     esbuildOptions: {
       resolveExtensions: extensions,
       jsx: 'automatic',
       loader: {
         '.js': 'jsx',
-        '.ts': 'tsx',
-        '.tsx': 'tsx',
-        '.jsx': 'jsx',
       },
     },
-  },
-  esbuild: {
-    include: /\.[jt]sx?$/,
-    exclude: [],
-    loader: 'tsx',
   },
 });
