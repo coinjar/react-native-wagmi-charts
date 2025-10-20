8a755eafb24a05efc7d77a237cc06a2b437b183d
Simon Schippl <simon.schippl@coinjar.com>
2025-10-20 13:31:35 +1100

    Add empty data validation

--- src/charts/candle/utils/getDomain.ts
+++ src/charts/candle/utils/getDomain.ts
@@ -2,6 +2,7 @@ import type { TCandle } from '../types';
 
 export function getDomain(rows: TCandle[]): [min: number, max: number] {
   'worklet';
+  if (rows.length === 0) return [0, 0];
   const values = rows.map(({ high, low }) => [high, low]).flat();
   const min = Math.min(...values);
   const max = Math.max(...values);
--- src/charts/line/utils/getDomain.ts
+++ src/charts/line/utils/getDomain.ts
@@ -2,6 +2,7 @@ import type { TLineChartPoint } from '../types';
 
 export function getDomain(rows: TLineChartPoint[]): [number, number] {
   'worklet';
+  if (rows.length === 0) return [0, 0];
   const values = rows.map(({ value }) => value);
   return [Math.min(...values), Math.max(...values)];
 }
