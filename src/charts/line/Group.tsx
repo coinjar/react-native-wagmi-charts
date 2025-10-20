8d2ad590621635551b61d9db66b4f87f62ac1aa3
Simon Schippl <simon.schippl@coinjar.com>
2025-10-20 13:35:28 +1100

    Clean up code (#221)
    * Remove .gitattributes file
    
    * Add empty data validation
    
    * Improve type safety

--- src/charts/line/Group.tsx
+++ src/charts/line/Group.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode, Children, cloneElement } from 'react';
 import { ViewProps, View } from 'react-native';
 import flattenChildren from 'react-keyed-flatten-children';
 
-import { LineChart } from './Chart';
+import { LineChart, LineChartProps } from './Chart';
 
 type Props = {
   children: ReactNode;
@@ -13,13 +13,16 @@ export function LineChartGroup({ children, ...props }: Props) {
   const flatChildrenCount = Children.count(flatChildren);
   return (
     <View {...props}>
-      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-      {Children.map(flatChildren, (child: any, index) => {
+      {Children.map(flatChildren, (child, index) => {
         const isLast = index === flatChildrenCount - 1;
-        if (!isLast && child.type === LineChart) {
+        if (
+          !isLast &&
+          React.isValidElement(child) &&
+          child.type === LineChart
+        ) {
           return cloneElement(child, {
             absolute: true,
-          });
+          } as Partial<LineChartProps>);
         }
         return child;
       })}
